1b8da9b251695fc4b118b39ff302d62b4ac74e57
Sulthan Laksono Ramadhan <58428798+Than1st@users.noreply.github.com>
2023-10-18 02:10:32 +0700

    Add files

---
+++ ReactExercise - Copy/client/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import './App.css';
+import {Home} from "./components/Home";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Navbar} from "./components/Navbar";
+import {AddItem} from "./components/AddItem";
+import {EditItem} from "./components/EditItem";
+
+function App() {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Navbar/>}>
+                        <Route index element={<Home/>}/>
+                        <Route path='/addItems' element={<AddItem/>}/>
+                        <Route path='/editItems'>
+                            <Route path=':id' element={<EditItem/>}/>
+                        </Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
